7ea42638c2811480c24d392b890742625a128a10
Alexandra <aleksa.fom@gmail.com>
2016-04-24 23:19:23 +0200

    sql

---
+++ src/main/resources/sql/sec-level.sql
@@ -0,0 +1,4 @@
+INSERT INTO SecurityLevel (id, level, value) VALUES (1, 0, 'no secret');
+INSERT INTO SecurityLevel (id, level, value) VALUES (2, 1, 'dsp');
+INSERT INTO SecurityLevel (id, level, value) VALUES (3, 2, 'secret');
+INSERT INTO SecurityLevel (id, level, value) VALUES (4, 3, 'SS');
---
+++ src/main/resources/sql/users.sql
@@ -0,0 +1,4 @@
+INSERT INTO User (id, login, name, password, securityLevel_id) VALUES (1, 'petrovna', 'Petrovna', '1234', 1);
+INSERT INTO User (id, login, name, password, securityLevel_id) VALUES (2, 'admin', 'Admin', 'admin', 4);
+INSERT INTO User (id, login, name, password, securityLevel_id) VALUES (3, 'user', 'User', 'user', 2);
+INSERT INTO User (id, login, name, password, securityLevel_id) VALUES (4, 'usersec', 'Secret user', 'user', 3);
